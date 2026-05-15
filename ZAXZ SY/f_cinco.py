84c3fadb61667aed5a2b305821c475dcb79ed2ea
DELTA <d3lt4.1nck@gmail.com>
2026-05-15 15:22:37 -0300

    Código 7 - V2

--- ZAXZ SY/f_cinco.py
+++ ZAXZ SY/f_cinco.py
@@ -1,5 +1,5 @@
 # CÓDIGO 7 - OPÇÃO 5
-# V.1 - Cada versão de cada código que não faça parte do sistema principal contribui em 0.1+ da versão principal.
+# V.2 - Cada versão de cada código que não faça parte do sistema principal contribui em 0.1+ da versão principal.
 # Possíveis retornos: 0, 1, 2, 6
 
 # Bloco de inicialização:
@@ -59,13 +59,13 @@ try:
                 elif selecionar_coisas in lista_tarefas:
                     print_slow(vermelho() + "\nEssa tarefa já existe.\n")
                     continue
-                elif len(selecionar_coisas) <= 3 or len(selecionar_coisas) >= 35:
-                    print_slow(vermelho() + "\nTarefa curta/grande demais.\n")
-                    continue
                 elif selecionar_coisas == "":
                     print_slow(verde() + "\nVamos iniciar...\n")
                     dormir(2)
                     break
+                elif len(selecionar_coisas) <= 3 or len(selecionar_coisas) >= 35:
+                    print_slow(vermelho() + "\nTarefa curta/grande demais.\n")
+                    continue
                 else:
                     print_slow(verde() + "\nTarefa anotada.\n")
                     print_slow(verde() + "\nCaso queira iniciar o sorteio, basta apertar 'Enter'.\n")
@@ -91,3 +91,4 @@ except Exception as erro_cruel_f_cinco:
         return 6 # Desconhecido
     
 # VERIFICADO: TUDO CERTO - FINALIZADO - V1.
+# VERIFICADO: TUDO CERTO - FINALIZADO - V2.
